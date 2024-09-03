7f8ce3376d4083aa2ad0013a09070af25b63331d
pssgiasmim <iasmimpassing@gmail.com>
2024-09-03 14:25:19 -0300

    ManagerUI

--- Campo Minado/Assets/Scrip/ManagerUI.cs
+++ Campo Minado/Assets/Scrip/ManagerUI.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Manager : MonoBehaviour
+public class ManagerUI : MonoBehaviour
 {
+    //Método que altera as propriedades de uma imagem
     [SerializeField] Image barraDeDificuldade;
     [SerializeField] Gradient corDaBarra;
 
     public void AtualizarBarra(float value)
     {
+        //o fillAmount faz a barrinha ter uma progressão, o qunato a barra vai ser preenchida
         barraDeDificuldade.fillAmount = value;
+
+        //Vai fazer ter um gradiente entre cores na barra
         barraDeDificuldade.color = corDaBarra.Evaluate(value);
     }
 }
